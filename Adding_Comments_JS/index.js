4830b12f449bdfc45582b38ba20065038ac2e0f7
Parvesh <82137941+Parveshcloud@users.noreply.github.com>
2022-04-03 22:53:01 +0530

    Adding_Comments_JS

---
+++ Adding_Comments_JS/index.js
@@ -0,0 +1,6 @@
+function addComments(id){
+        var addEventName = id.value +'<br>';
+        var a = document.getElementById('userComments');
+        document.getElementById('addEventNames').innerHTML += '<p>'+addEventName+'</p>';          
+        a.value=a.defaultValue;
+    }
